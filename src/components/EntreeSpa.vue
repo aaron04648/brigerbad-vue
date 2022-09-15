3b039cdbaca69f05e7540b781adeca31ee502ccd
Fabio Bumann <Fabio Bumann>
2022-09-15 16:30:42 +0200

    Sliders

--- src/components/EntreeSpa.vue
+++ src/components/EntreeSpa.vue
@@ -1,22 +1,12 @@
 <template>
-<div  :style="{
-    
-   
-      'position':'absolut',
-      'width':'100vw',
-      'height':'100vh',
-      'top':'0',
-      'left':'0',
-      'bottom':'0',
-      'right':'0',
-      'overflow':'hidden',
-      'scroll':'no',
+<div class="mainmain">
 
-
-      'margin':'0'    }">
+</div>
+<div class="Main">
       <div class="cardcontainer">
             <img class="galleryImg" src="../assets/terme-1.jpg">
         </div>
+</div>
   <table id="div-main">
   <tr>
     <th class="th-1">Eintritt Spa</th>
@@ -30,8 +20,13 @@
   </tr>
   
 </table>
+
+
+<div class="Main2">
+      <div class="cardcontainer">
+            <img class="galleryImg" src="../assets/spa-1.jpg">
+        </div>
 </div>
-   
 </template>
 
 <script>
@@ -41,11 +36,45 @@ data(){
         return{
             data: jsonData
         }
-    }
+    },mounted(){}
 }
 </script>
 
 <style scoped>
+.mainmain{
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+
+.Main{
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+  margin: 0;
+  opacity: 0;
+  animation: mainhidden1 30s infinite ;
+}
+
+.Main2{
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+  margin: 0;
+  opacity: 0;
+  animation: mainhidden2 30s infinite ;
+}
 
 th {
   font-weight: bold;
@@ -61,11 +90,13 @@ table {
 }
 
 #div-main {
+  position: fixed;
   color: white;
   padding: 2vw;
   margin-top:25vw ;
   margin-left: 3.6458333333333335vw;
   font-size: 4.2vw;
+  z-index: 1;
   background-color: rgb(0, 0, 0, 0.8);
 }
 .th-1 {
@@ -95,21 +126,66 @@ table {
   padding-right:2.5vw ;
 
 }
-  @keyframes zoominoutsinglefeatured {
-                0% {
-                    height: 100vh;
-                    transform: scale(1,1);
-                }
-                100% {
-                    height: 100vh;
-                    transform: scale(1.2,1.2);
-                }
-            }
-            .cardcontainer img {
-                position: fixed;
-                left: -100vw;
-                float: left;
-                z-index: -1;
-                animation: zoominoutsinglefeatured 8s infinite ;
-            }
+@keyframes zoominoutsinglefeatured {
+  0% {
+      height: 100vh;
+      transform: scale(1,1);
+  }
+  100% {
+      height: 100vh;
+      transform: scale(1.2,1.2);
+  }
+}
+
+@keyframes mainhidden1 {
+  0% {
+      opacity: 0;
+  }
+  7.5% {
+      opacity: 1;
+  }
+  10% {
+      opacity: 1;
+  }
+  49% {
+      opacity: 1;
+  }
+  50%{
+      opacity: 0;
+  }
+  100%{
+      opacity: 0;
+  }
+}
+
+@keyframes mainhidden2 {
+  0% {
+      opacity: 0;
+  }
+  10% {
+      opacity: 0;
+  }
+  50% {
+      opacity: 0;
+  }
+  51%{
+      opacity: 1;
+  }
+  92.5%{
+      opacity: 1;
+  }
+  100%{
+      opacity: 0;
+  }
+}
+
+  .cardcontainer img {
+      position: fixed;
+      left: -100vw;
+      float: left;
+      z-index: -1;
+      animation: zoominoutsinglefeatured infinite ;
+      animation-duration: 15s;
+      animation-timing-function: linear;
+  }
 </style>
